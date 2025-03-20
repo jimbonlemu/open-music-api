ad33d58bb14e9fd77b7f462e9b498b5520ff717d
jimbonlemu <iqbaltech29@gmail.com>
2025-03-20 19:12:01 +0700

    Add initial project setup with server configuration, ESLint, and .gitignore

---
+++ src/server.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const AlbumsService = require('./services/postgres/AlbumService');
+const SongsService = require('./services/postgres/SongService');
+const albumValidator = require('./validator/albums');
+const songValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
+
+const init = async () => {
+    const albumsService = new AlbumsService();
+    const songsService = new SongsService();
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.register(
+        [{
+            plugin: albums,
+            options: {
+                service: albumsService,
+                validator: albumValidator,
+            }
+        },
+        {
+            plugin: songs,
+            options: {
+                service: songsService,
+                validator: songValidator,
+            }
+        },],
+    );
+
+    server.ext('onPreResponse', (request, h) => {
+        const { response } = request;
+
+        if (response instanceof ClientError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: response.message,
+            });
+
+            newResponse.code(response.statusCode);
+            return newResponse;
+        }
+
+        return h.continue;
+    });
+
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
+
+}
+
+init();
