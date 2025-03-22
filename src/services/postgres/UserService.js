e7bc85de0e4e66c4f4db0eb166649f172965735d
jimbonlemu <iqbaltech29@gmail.com>
2025-03-23 01:41:19 +0700

    Add createdAt timestamp to user records during registration

--- src/services/postgres/UserService.js
+++ src/services/postgres/UserService.js
@@ -2,20 +2,21 @@ const { nanoid } = require("nanoid");
 const pool = require("../../utils/pool");
 const bcrypt = require('bcrypt');
 const { InvariantError } = require('../../exceptions/index');
-
+const { getCurrentTimestamp } = require('../../utils/dateUtils');
 class UserService {
     constructor() {
         this._pool = pool;
     }
-    
+
     async addUser({ username, password, fullname }) {
         await this.verifyUsername(username);
         const id = `user-${nanoid(16)}`;
+        const createdAt = getCurrentTimestamp();
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const query = {
-            text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
-            values: [id, username, hashedPassword, fullname],
+            text: 'INSERT INTO users VALUES($1, $2, $3, $4,$5, $6) RETURNING id',
+            values: [id, username, hashedPassword, fullname, createdAt, createdAt],
         };
 
         const result = await this._pool.query(query);
